d7bcbf3fb4dc7cb6fcd4f8c20f981702e3813d57
Ideal Kendall <antonio.kendall@gmail.com>
2024-03-30 23:26:28 -0400

    6. Token Approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -36,10 +36,7 @@ contract Token {
         uint256 _value
     ) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
-        require(_to != address(0));
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
+        _transfer(msg.sender, _to, _value);
         return true;
     }
 
@@ -51,5 +48,30 @@ contract Token {
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        require(_to != address(0));
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        //require(_value <= balanceOf[_from]);
+        require(_to != address(0));
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
     }
 }
