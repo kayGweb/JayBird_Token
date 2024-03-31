20b8c364c03cb04a7b41725dbaa5f89ff3fec066
Ideal Kendall <antonio.kendall@gmail.com>
2024-03-30 22:35:14 -0400

    5. token Transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -12,7 +12,7 @@ contract Token {
     //track balances
     mapping(address => uint256) public balanceOf;
 
-    //send tokens
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     constructor(
         string memory _name,
@@ -23,5 +23,17 @@ contract Token {
         symbol = _symbol;
         totalSupply = _totalSupply * 10 ** decimals;
         balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        require(_to != address(0));
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -9,6 +9,7 @@ describe("Token", () => {
 	let token;
 	let deployer;
 	let accounts;
+	let receiver;
 
 	beforeEach(async () => {
 		//Fetch Token from blockchain
@@ -17,6 +18,7 @@ describe("Token", () => {
 		//get the accouts
 		accounts = await ethers.getSigners();
 		deployer = accounts[0];
+		receiver = accounts[1];
 	});
 
 	describe("Deployment", async () => {
@@ -25,22 +27,22 @@ describe("Token", () => {
 		const totalSupply = tokens(1000000);
 		const decimals = 18;
 
-		it("Has a correct name", async () => {
+		it("has a correct name", async () => {
 			//Check that the name is correct
 			expect(await token.name()).to.be.eq(name);
 		});
 
-		it("Has a correct symbol", async () => {
+		it("has a correct symbol", async () => {
 			//Check that the symbol is correct
 			expect(await token.symbol()).to.be.eq(symbol);
 		});
 
-		it("Has a correct decimals", async () => {
+		it("has a correct decimals", async () => {
 			//Check that the decimals is correct
 			expect(await token.decimals()).to.be.eq(decimals);
 		});
 
-		it("Has a correct totalSupply", async () => {
+		it("has a correct totalSupply", async () => {
 			//Check that the totalSupply is correct
 			expect(await token.totalSupply()).to.be.eq(totalSupply);
 		});
@@ -48,6 +50,48 @@ describe("Token", () => {
 		it("assigns total supply to deployer", async () => {
 			//Check that the balanceOf is assigned totalSupply
 			expect(await token.balanceOf(deployer.address)).to.be.eq(totalSupply);
+		});
+	});
+
+	describe("Sending Tokens", async () => {
+		let amount, transaction, result;
+
+		describe("Success", async () => {
+			beforeEach(async () => {
+				//transfer tokens to receiver
+				amount = tokens(100);
+				transaction = await token.connect(deployer).transfer(receiver.address, amount);
+				result = await transaction.wait();
+			});
+
+			it("transfer tokens balances", async () => {
+				//Ensure that tokens were transferred
+				expect(await token.balanceOf(deployer.address)).to.be.eq(tokens(999900));
+				expect(await token.balanceOf(receiver.address)).to.be.eq(amount);
+			});
+
+			it("emits Transfer event", async () => {
+				//Ensure that the Transfer event was emitted
+				const event = result.events[0];
+				expect(event.event).to.be.eq("Transfer");
+
+				const args = event.args;
+				expect(args.from).to.be.eq(deployer.address);
+				expect(args.to).to.be.eq(receiver.address);
+				expect(args.value).to.be.eq(amount);
+			});
+		});
+
+		describe("Failure", async () => {
+			it("rejects insufficent balances", async () => {
+				//Attempt to transfer more tokens than the balance
+				const invalidAmount = tokens(100000000);
+				await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted;
+			});
+			it("rejects invalid recipients", async () => {
+				const amount = tokens(100);
+				await expect(token.connect(deployer).transfer("0x0000000000000000000000000000000000000000", amount)).to.be.reverted;
+			});
 		});
 	});
 });
